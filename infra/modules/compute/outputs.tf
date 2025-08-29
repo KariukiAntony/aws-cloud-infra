008d96050a44b72207a05238926a51be010f7fc9
KariukiAntony <antonygichoya9@gmail.com>
2025-08-29 19:30:18 +0300

    Feat: Cloudwatch alarms in monitoring module responsible for scalingin/out

--- infra/modules/compute/outputs.tf
+++ infra/modules/compute/outputs.tf
@@ -14,12 +14,17 @@ output "alb_zoneid" {
   value       = aws_lb.main.zone_id
 }
 
-# output "scale_up_policy_arn" {
-#   description = "ARN of the scale up policy"
-#   value       = aws_autoscaling_policy.scale_up.arn
-# }
+output "scale_up_policy_arn" {
+  description = "ARN of the scale up policy"
+  value       = aws_autoscaling_policy.scale_up.arn
+}
 
-# output "scale_down_policy_arn" {
-#   description = "ARN of the scale down policy"
-#   value       = aws_autoscaling_policy.scale_down.arn
-# }
+output "scale_down_policy_arn" {
+  description = "ARN of the scale down policy"
+  value       = aws_autoscaling_policy.scale_down.arn
+}
+
+output "autoscaling_group_name" {
+  description = "Name of ASG"
+  value       = aws_autoscaling_group.main.name
+}
